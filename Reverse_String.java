be753a833402393d31edda1c6fe64feb6ac5069d
Kevin Cox <kevinpetercox@gmail.com>
2016-09-19 17:36:33 +0100

    Added Reverse_String.java
    Reverses a String input

---
+++ Reverse_String.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Reverse_String {
+	
+	private static Scanner reader;
+
+	public static String reverseString(String input){
+		String[] array = input.split("(?!^)");
+		
+		for(int i = 0; i< input.length()/ 2; i++){
+			String temp = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = temp;
+		}
+		
+		return String.join("",array);
+	}
+	
+	public static void main(String [] args){
+		reader = new Scanner(System.in);
+		String input = reader.nextLine();
+		System.out.println(reverseString(input));
+	}
+
+}
